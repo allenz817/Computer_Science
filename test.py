405662963c574c21d396dae1d23323e83662a45e
allenz817 <allenjzhao@gmail.com>
2024-09-15 12:10:13 +0800

    modified

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("hello world")
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        last = len(self.items)-1
+        return self.items[last]
+    
+    def size(self):
+        return len(self.items)
+    
+stack = Stack()
+print(stack.is_empty())
