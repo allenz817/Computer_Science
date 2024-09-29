76f7cc849e076aed8d77b981703d003556f7fbc8
allenz817 <allenjzhao@gmail.com>
2024-09-29 10:51:46 +0800

    240929

--- temp.py
+++ temp.py
@@ -1,4 +1,19 @@
-my_dict = {(1): ['qwe', 'rt', 'y']}
-result_dict = {word: len(word) for word in my_dict[1]}
-print(result_dict)
+class Rectangle():
+    recs = []
 
+    def __init__(self, w, l):
+        self.width = w
+        self.len = l
+        self.recs.append((self.width,
+                          self.len))
+
+    def print_size(self):
+        print("""{} by {}
+              """.format(self.width,
+                         self.len))
+
+r1 = Rectangle(10, 24)
+r2 = Rectangle(20, 40)
+r3 = Rectangle(100, 200)
+
+print(Rectangle.recs)
