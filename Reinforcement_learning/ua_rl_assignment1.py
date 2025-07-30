bb2c023407c40fc9f0780d286e4e1b81b0089387
allenz817 <allenjzhao@gmail.com>
2025-07-30 20:58:03 +0800

    250730

---
+++ Reinforcement_learning/ua_rl_assignment1.py
@@ -0,0 +1,110 @@
+# Import necessary libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+
+from rlglue.rl_glue import RLGlue
+import main_agent
+import ten_arm_env
+import test_env
+
+def argmax(q_values):
+    """
+    Takes in a list of q_values and returns the index of the item 
+    with the highest value. Breaks ties randomly.
+    returns: int - the index of the highest value in q_values
+    """
+    """
+    #alternative code
+    max_value = max(q_values)
+    max_indices = [i for i, v in enumerate(q_values) if v == max_value]
+    return np.random.choice(max_indices)
+    """
+    top_value = float('-inf')
+    ties = []
+    
+    for i in range(len(q_values)):
+        if q_values[i] > top_value:
+            top_value = q_values[i]
+            ties = [i]
+        elif q_values[i] == top_value:
+            ties.append(i)
+    return np.random.choice(ties)
+
+class GreedyAgent(main_agent.Agent):
+    def agent_step(self, reward, observation=None):
+        """
+        Takes one step for the agent. It takes in a reward and observation and 
+        returns the action the agent chooses at that time step.
+        
+        Arguments:
+        reward -- float, the reward the agent recieved from the environment after taking the last action.
+        observation -- float, the observed state the agent is in. Do not worry about this as you will not use it
+                              until future lessons
+        Returns:
+        current_action -- int, the action chosen by the agent at the current time step.
+        """
+        ### Useful Class Variables ###
+        # self.q_values : An array with what the agent believes each of the values of the arm are.
+        # self.arm_count : An array with a count of the number of times each arm has been pulled.
+        # self.last_action : The action that the agent took on the previous time step
+        #######################
+        
+        # Update Q values Hint: Look at the algorithm in section 2.4 of the textbook.
+        # increment the counter in self.arm_count for the action from the previous time step
+        # update the step size using self.arm_count
+        # update self.q_values for the action from the previous time step
+
+        current_action = argmax(self.q_values)
+        if self.last_action is not None:
+            self.arm_count[self.last_action] += 1
+            step_size = 1 / self.arm_count[self.last_action]
+            self.q_values[self.last_action] += step_size * (reward - self.q_values[self.last_action])
+        
+        else:
+            # If this is the first step, initialize the arm_count for the current action
+            self.arm_count[current_action] = 1
+            self.q_values[current_action] = 0
+            
+        self.last_action = current_action
+        
+        return current_action
+    
+# ---------------
+# Discussion Cell
+# ---------------
+
+num_runs = 200                    # The number of times we run the experiment
+num_steps = 1000                  # The number of pulls of each arm the agent takes
+env = ten_arm_env.Environment     # We set what environment we want to use to test
+agent = GreedyAgent               # We choose what agent we want to use
+agent_info = {"num_actions": 10}  # We pass the agent the information it needs. Here how many arms there are.
+env_info = {}                     # We pass the environment the information it needs. In this case nothing.
+
+rewards = np.zeros((num_runs, num_steps))
+average_best = 0
+for run in tqdm(range(num_runs)):           # tqdm is what creates the progress bar below
+    np.random.seed(run)
+    
+    rl_glue = RLGlue(env, agent)          # Creates a new RLGlue experiment with the env and agent we chose above
+    rl_glue.rl_init(agent_info, env_info) # We pass RLGlue what it needs to initialize the agent and environment
+    rl_glue.rl_start()                    # We start the experiment
+
+    average_best += np.max(rl_glue.environment.arms)
+    
+    for i in range(num_steps):
+        reward, _, action, _ = rl_glue.rl_step() # The environment and agent take a step and return
+                                                 # the reward, and action taken.
+        rewards[run, i] = reward
+
+greedy_scores = np.mean(rewards, axis=0)
+plt.figure(figsize=(15, 5), dpi= 80, facecolor='w', edgecolor='k')
+plt.plot([average_best / num_runs for _ in range(num_steps)], linestyle="--")
+plt.plot(greedy_scores)
+plt.legend(["Best Possible", "Greedy"])
+plt.title("Average Reward of Greedy Agent")
+plt.xlabel("Steps")
+plt.ylabel("Average reward")
+plt.show()
--- temp.py
+++ temp.py
@@ -1,3 +1,18 @@
-from itertools import combinations_with_replacement
-for c in combinations_with_replacement("TBL", 4):
-    print("".join(c))
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'folder_with_twttr'))
+import pytest
+import twttr
+
+def test_word():
+    input = "twitter"
+    assert twttr.shorten(input) == "twttr"
+
+def test_sentence():
+    input = "What's your name?"
+    assert twttr.shorten(input) == "Wht's yr nm?"
+
+def test_number():
+    input = "CS50"
+    assert twttr.shorten(input) == "CS50"
+
