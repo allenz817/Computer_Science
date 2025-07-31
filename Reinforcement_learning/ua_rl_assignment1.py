611c3c5f6d922001e54122fdd5c2bf8dd60cd941
allenz817 <allenjzhao@gmail.com>
2025-07-31 20:37:47 +0800

    250731

--- Reinforcement_learning/ua_rl_assignment1.py
+++ Reinforcement_learning/ua_rl_assignment1.py
@@ -71,7 +71,8 @@ class GreedyAgent(main_agent.Agent):
         self.last_action = current_action
         
         return current_action
-    
+
+"""
 # ---------------
 # Discussion Cell
 # ---------------
@@ -108,3 +109,79 @@ plt.title("Average Reward of Greedy Agent")
 plt.xlabel("Steps")
 plt.ylabel("Average reward")
 plt.show()
+"""
+
+class EpsilonGreedyAgent(main_agent.Agent):
+    def agent_step(self, reward, observation):
+        """
+        Takes one step for the agent. It takes in a reward and observation and 
+        returns the action the agent chooses at that time step.
+        
+        Arguments:
+        reward -- float, the reward the agent recieved from the environment after taking the last action.
+        observation -- float, the observed state the agent is in. Do not worry about this as you will not use it
+                              until future lessons
+        Returns:
+        current_action -- int, the action chosen by the agent at the current time step.
+        """
+        
+        ### Useful Class Variables ###
+        # self.q_values : An array with what the agent believes each of the values of the arm are.
+        # self.arm_count : An array with a count of the number of times each arm has been pulled.
+        # self.last_action : The action that the agent took on the previous time step
+        # self.epsilon : The probability an epsilon greedy agent will explore (ranges between 0 and 1)
+        #######################
+        
+        # Update Q values - this should be the same update as your greedy agent above
+        if self.last_action is not None:
+            self.arm_count[self.last_action] += 1
+            step_size = 1 / self.arm_count[self.last_action]
+            self.q_values[self.last_action] += step_size * (reward - self.q_values[self.last_action])
+        
+        # Choose action using epsilon greedy
+        if np.random.random() < self.epsilon:
+            current_action = np.random.choice(len(self.q_values))
+        else:
+            current_action = argmax(self.q_values)
+
+        self.last_action = current_action
+        
+        return current_action
+    
+class EpsilonGreedyAgentConstantStepsize(main_agent.Agent):
+    def agent_step(self, reward, observation):
+        """
+        Takes one step for the agent. It takes in a reward and observation and 
+        returns the action the agent chooses at that time step.
+        
+        Arguments:
+        reward -- float, the reward the agent recieved from the environment after taking the last action.
+        observation -- float, the observed state the agent is in. Do not worry about this as you will not use it
+                              until future lessons
+        Returns:
+        current_action -- int, the action chosen by the agent at the current time step.
+        """
+        
+        ### Useful Class Variables ###
+        # self.q_values : An array with what the agent believes each of the values of the arm are.
+        # self.arm_count : An array with a count of the number of times each arm has been pulled.
+        # self.last_action : An int of the action that the agent took on the previous time step.
+        # self.step_size : A float which is the current step size for the agent.
+        # self.epsilon : The probability an epsilon greedy agent will explore (ranges between 0 and 1)
+        #######################
+        
+        # Update q_values for action taken at previous time step 
+        # using self.step_size intead of using self.arm_count
+        if self.last_action is not None:
+            self.q_values[self.last_action] += self.step_size * (reward - self.q_values[self.last_action])
+
+
+        # Choose action using epsilon greedy. This is the same as you implemented above.
+        if np.random.random() < self.epsilon:
+            current_action = np.random.choice(len(self.q_values))
+        else:
+            current_action = argmax(self.q_values)
+
+        self.last_action = current_action
+        
+        return current_action
